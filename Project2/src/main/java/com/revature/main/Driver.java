11624e29f5928372de3f07107a1b29a1cf72fb12
Lucas Desktop <lgc13@my.fsu.edu>
2017-07-08 18:20:35 -0400

    jonathan files from lucas

--- Project2/src/main/java/com/revature/main/Driver.java
+++ Project2/src/main/java/com/revature/main/Driver.java
@@ -1,10 +1,18 @@
 package com.revature.main;
 
+
+
+import com.revature.bean.Roles;
+import com.revature.dao.UserDao;
+import com.revature.dao.UserDaoImpl;
+
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		UserDao dao = new UserDaoImpl();
+		
+		
+		dao.createUser("Jonathan", test);
 	}
 
 }
