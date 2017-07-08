478ee90bd8850134b95ad9edf171d1ca8ae1f144
Lucas Desktop <lgc13@my.fsu.edu>
2017-07-08 15:22:04 -0400

    adding LucasBranch files

--- Project2/src/main/webapp/script.js
+++ Project2/src/main/webapp/script.js
@@ -3,16 +3,12 @@
  * module is a container for the different parts of application (controllers, service, etc)
  * 
  * ngRoute will make it possible to not refresh the page (its a module)
- */
-									
+ */									
 var myApp = angular.module('myApp', ['ngRoute']); 
 
-myApp.controller('mainController', function($scope) {
-	
-	$scope.message = 'Hello from home controller';
-	
-});
-
+/*
+ * DEFINING CONTROLLERS
+ */
 myApp.config(function($routeProvider) {
   $routeProvider
 
@@ -26,14 +22,17 @@ myApp.config(function($routeProvider) {
     controller  : 'aboutController'
   })
 
-  .when('/contact', {
-    templateUrl : 'pages/contact.html',
-    controller  : 'contactController'
+  .when('/rules', {
+    templateUrl : 'pages/rules.html',
+    controller  : 'rulesController'
   })
 
   .otherwise({redirectTo: '/'});
 });
 
+/*
+ * CONTROLLERS METHODS
+ */
 //this will remove the URL prefix
 myApp.config(['$locationProvider', function($locationProvider) {
 	  $locationProvider.hashPrefix('');
@@ -42,18 +41,30 @@ myApp.config(['$locationProvider', function($locationProvider) {
 //create the controller and inject Angular's $scope
 myApp.controller('mainController', function($scope) {
 
-    // create a message to display in our view
-    $scope.message = 'This is the home page';
+    $scope.message = 'This is the home page, from script.js'; // create a message to display in our view
 });
+
 
 myApp.controller('aboutController', function($scope) {
 	
-	$scope.message = "Now I'm in the about page";
+	$scope.message = "Now I'm in the about page, from script.js";
 });
 
-myApp.controller('contactController', function($scope) {
-	$scope.message = "here is the contact page";
+
+myApp.controller('rulesController', function($scope) {
+	
+	$scope.message = "here is the rules page, from script.js";
+	$scope.message2 = "Message 2 information here..., from scrip.js";
 });
 
+
+//this will make the navbar active for the nav being clicked
+myApp.controller('NavController', function NavController($scope, $location) {   
+	
+	$scope.isActive = function (viewLocation)
+	{
+		return viewLocation === $location.path();
+	};
+})
 
 
