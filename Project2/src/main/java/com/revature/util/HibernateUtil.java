e241552fbb52e9314aa13b5e63b4e44f50cfbfbf
LucasC <lucas.costa.usa@hotmail.com>
2017-07-12 11:56:15 -0400

    trying to get ec2 add user to work

--- Project2/src/main/java/com/revature/util/HibernateUtil.java
+++ Project2/src/main/java/com/revature/util/HibernateUtil.java
@@ -6,6 +6,7 @@ import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 	
+@SuppressWarnings("deprecation")
 private static SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 	
 	public static Session getSession(){
