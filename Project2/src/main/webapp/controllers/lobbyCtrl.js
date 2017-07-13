c94f9e306679dfe1f52780e16f3bebfc0ef6d361
LucasC <lucas.costa.usa@hotmail.com>
2017-07-13 15:45:41 -0400

    added loading circle, fixed css for all pages, moved footer to bottom, moved divs in lobby.html

--- Project2/src/main/webapp/controllers/lobbyCtrl.js
+++ Project2/src/main/webapp/controllers/lobbyCtrl.js
@@ -11,41 +11,41 @@ myApp.controller('LobbyController', ['$scope', '$http', function($scope, $http) 
 	
 	//$scope.allPlayers = ['test1', 'test2'];
 			
+	/*var myVar;
+
+	$scope.hideFunction = function() {
+		console.log("In hideFunction");
+	    myVar = setTimeout(showPage, 3000);
+	}
+
+	function showPage() {
+	  document.getElementById("loader").style.display = "none";
+	  document.getElementById("myDiv").style.display = "block";
+	}*/
+	
 	console.log("Trying to get users from DB");
+	$scope.loadingRequest = true;
+	
 	$http({
 		url: '/Project2/lobby.do',
 		method: 'GET',
 	}).then(function successCallBack(response) {
 		$scope.allPlayers = response.data;
 		console.log("successfully got players");
-		console.log("vm.allPLayers: " + $scope.allPlayers)
+		console.log("scope.allPLayers: " + $scope.allPlayers)
+		
+		$scope.loadingRequest = false;
+		
 	}, function errorCallBack(response){
 		console.log("did not get players")
+		
+		$scope.loadingRequest = false;
+		$scope.message = "There are no other players online."
 	});
 		
 	
 }]);
 
-/*promise.then(function(response) {
-allRoomsData.allRooms = response.data;
-ForumService.allPlayers = response.data;
-}, function(response) {
-
-});*/
-/*myApp.service("GetPlayersService", function($http, allPlayers){
-	
-	var playerData = this;
-	
-	playerData.allPlayers = [];
-	
-	playerData.getAllPlayers = function(){
-		return $http({
-			method: 'GET',
-			url: 'Project2/lobby'
-		});
-	}
-});
-*/
 
 
 
