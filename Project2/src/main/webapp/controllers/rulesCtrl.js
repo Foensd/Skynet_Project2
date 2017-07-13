77b5b73371ee0d975df1e4688806696a17090d6d
LucasC <lucas.costa.usa@hotmail.com>
2017-07-13 18:01:04 -0400

    added play Ctrl and html file, make some minor Css changes

--- Project2/src/main/webapp/controllers/rulesCtrl.js
+++ Project2/src/main/webapp/controllers/rulesCtrl.js
@@ -9,10 +9,11 @@ myApp.controller('RulesController', function($scope, $interval) {
 	$scope.message = "here is the rules page, from script.js";
 	$scope.message2 = "Message 2 information here... from homeCtrl.js";
 	
+	/* David was trying out a counter
 	$scope.counter = 0;
 	$interval(function(){
 		console.log("Iteration: " + $scope.counter);
 		$scope.counter = $scope.counter + 1;
-		}, 500, 15);
+		}, 500, 15);*/
 });
 
