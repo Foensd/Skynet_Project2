59b3e0317c852de3f95ab99e91c905c038ac0d7f
LucasC <lucas.costa.usa@hotmail.com>
2017-07-13 18:40:05 -0400

    Added loading circle to register.html

--- Project2/src/main/webapp/controllers/registerCtrl.js
+++ Project2/src/main/webapp/controllers/registerCtrl.js
@@ -18,19 +18,28 @@ myApp.controller('RegisterController', [
 				username : ''
 			};
 
+			$scope.loadingRequest = true; // makes the loadingRequest true, so it will display 'loader' upon page load
+			
 			$scope.register = function() {
-			
+				$scope.loadingRequest = false; // set loadingRequest to false so it hides the input box
+				$scope.userMessage = "One second... ";
+				
 				playerObject = $scope.user; // adding user to a playerObject
-				console.log('playerObject.username: ' + playerObject.username);
-			
 				console.log("REGISTER BUTTON WAS CLICKED");
-				$http.post(REQUEST_SERVICE_URI, playerObject).then(
-						function successsCallBack(response) {
-							var path = "/Project2/#/lobby";
-							location.href = path;
-						}, function() {
-							alert("Username is either taken or blank");
-						});
+					
+				$http.post(REQUEST_SERVICE_URI, playerObject)
+					.then(function successsCallBack(response) {
+						$scope.userMessage = "Done! See, that wasn't so bad ;)";
+						
+						console.log("Creating new user(playerObject) with username: " + playerObject.username)
+						
+						
+						var path = "/Project2/#/lobby";
+						location.href = path;
+					}, function errorCallBack(response) {
+						$scope.loadingRequest = true; // set loadingRequest back to true so it stops spinning, and lets user try again
+						alert("Username is either taken or blank");
+					});
 
 			}
 		} ])
