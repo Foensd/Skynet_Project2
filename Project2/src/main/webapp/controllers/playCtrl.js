4b8acbe9aacede9de2962091f54c5802a7ecce37
LucasC <lucas.costa.usa@hotmail.com>
2017-07-15 17:01:10 -0400

    added a crap ton of css to play.html

--- Project2/src/main/webapp/controllers/playCtrl.js
+++ Project2/src/main/webapp/controllers/playCtrl.js
@@ -3,10 +3,10 @@ var myApp = angular.module('myApp');
 /*
  * CONTROLLERS METHOD
  */
-myApp.controller('PlayController', ['$rootScope', '$scope', function($rootScope, $scope) {
+myApp.controller('PlayController', ['$http', '$rootScope', '$scope', function($http, $rootScope, $scope) {
 	
 	$scope.rolePredictions = ['Employee', 'Hacker', 'HR', 'Trainer'];
-	$scope.allPlayers = [
+	/*$scope.allPlayers = [
 		{name: 'Player 1', status: 'Active'},
 		{name: 'I have a really long name', status: 'Fired'},
 		{name: 'Player 3', status: 'Active'},
@@ -17,9 +17,32 @@ myApp.controller('PlayController', ['$rootScope', '$scope', function($rootScope,
 		{name: 'Player 4', status: 'Fired'},
 		{name: 'Player 4', status: 'Fired'},
 		{name: 'Player 4', status: 'Fired'}
-	];
+	];*/
 	
-	$scope.status = ['Employee', 'Fired'];
+	getPlayers = function() {
+		console.log("Trying to get users from DB");
+		
+		$http({
+			url: '/Project2/lobby.do',
+			method: 'GET',
+		})
+		.then(function successCallBack(response) {  // goes in DB and returns list with usernames if successful 
+			
+			$scope.allPlayers = response.data;
+			console.log("successfully got players");
+			console.log("scope.allPLayers: " + $scope.allPlayers)
+			$scope.numberOfPlayers = $scope.allPlayers.length;
+			$scope.loadingRequest = false;  // hide the 'loader'
+			
+		}, function errorCallBack(response){
+			console.log("did not get players")
+			
+			$scope.loadingRequest = false;
+			$scope.message = "There are no other players online."
+		});
+	}
+	
+	getPlayers(); // run getPlayers() function to retrieve players upon page load
    
 }]);
 
