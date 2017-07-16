7876073c3d4b7a61fac10868a68f02281ad08857
David Foens <Foensd@yahoo.com>
2017-07-16 02:25:07 -0400

    Updated started boolean on ready click

--- Project2/src/main/java/com/revature/resources/SpringController.java
+++ Project2/src/main/java/com/revature/resources/SpringController.java
@@ -87,6 +87,7 @@ public class SpringController {
 
 	@RequestMapping(method = RequestMethod.POST, headers = "Accept=application/json", value = "/readyButton.do")
 	public ResponseEntity<Void> readyButton(@RequestBody String jsonObject, HttpSession session) {
+		started = false;
 		Users user = null;
 		UserDao userDao = new UserDaoImpl();
 		System.out.println("jsonObject: " + jsonObject);
